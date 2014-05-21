507aa361ac14f5e3ea93f8c540239b4fd950b851
Travis Taylor <travis@webdesignofyork.com>
2014-05-21 18:53:48 -0400

    Add title

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Stuff Here</title>
 </head>
 <body>
+	<h1>Title</h1>
 	<div id="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae nemo expedita illum aliquam commodi delectus quae! Sint, hic sed dolorum minus nesciunt praesentium deleniti porro quia sunt non cumque.</div>
 </body>
 </html>
