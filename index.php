50421af327591b034e2f698d8e01ead12165d5b5
Travis Taylor <travis@webdesignofyork.com>
2014-05-21 19:03:56 -0400

    Add subtitle

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
 	<h1>Title</h1>
+	<h2>Subtitle</h2>
 	<div id="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae nemo expedita illum aliquam commodi delectus quae! Sint, hic sed dolorum minus nesciunt praesentium deleniti porro quia sunt non cumque.</div>
 </body>
 </html>
