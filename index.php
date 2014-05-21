d31b8d0dbab49aeec715c8de5796831f9520f76d
Travis Taylor <travis@webdesignofyork.com>
2014-05-21 18:49:26 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Stuff Here</title>
+</head>
+<body>
+	<div id="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae nemo expedita illum aliquam commodi delectus quae! Sint, hic sed dolorum minus nesciunt praesentium deleniti porro quia sunt non cumque.</div>
+</body>
+</html>
