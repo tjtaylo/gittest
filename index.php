bd36e9869a490581195cd2f2832f0d6ef7ca6b68
caleb-mil <calebdotmiller@gmail.com>
2014-05-21 19:50:32 -0400

    Add more content

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 	<h1>Title</h1>
 	<h2>Subtitle</h2>
 	<div id="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae nemo expedita illum aliquam commodi delectus quae! Sint, hic sed dolorum minus nesciunt praesentium deleniti porro quia sunt non cumque.</div>
+	<h2>Subtitle 2</h2>
+	<div class="bodycontent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae nemo expedita illum aliquam commodi delectus quae! Sint, hic sed dolorum minus nesciunt praesentium deleniti porro quia sunt non cumque.</div>
 </body>
 </html>
