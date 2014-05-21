e166e001ee61b9082f2f2deabd0f9e1f635d8c26
Travis Taylor <travis@webdesignofyork.com>
2014-05-21 19:52:22 -0400

    Another subtitle

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
 	<div id="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae nemo expedita illum aliquam commodi delectus quae! Sint, hic sed dolorum minus nesciunt praesentium deleniti porro quia sunt non cumque.</div>
 	<h2>Subtitle 2</h2>
 	<div class="bodycontent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae nemo expedita illum aliquam commodi delectus quae! Sint, hic sed dolorum minus nesciunt praesentium deleniti porro quia sunt non cumque.</div>
+	<h3>Another one!</h3>
 </body>
 </html>
